95b1fe61b7f9c1899ec6d476c1f504f0bbfe7812
Pieter <28593855@g.nwu.ac.za>
2019-09-01 18:35:54 +0200

    Add project files.

---
+++ StringManipulationTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace StringManipulationTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(12, 12);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(585, 196);
+            this.txtText.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 214);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(222, 88);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 314);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ StringManipulationTest/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringManipulationTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string text = txtText.Text;
+            string smallText = "";
+            string bigText = "";
+            int spaces = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] >= 97 && text[i] <= 109)        //if letter is between a and m (1 to 13)
+                {
+                    smallText += Convert.ToString(Convert.ToChar(text[i] + 13));     //Apply ROT13
+                }
+                else if (text[i] >= 110 && text[i] <= 122)      //if letter is between n and z (14 to 26)
+                {
+                    smallText += Convert.ToString(Convert.ToChar(text[i] - 13));     //Apply ROT13
+                }
+                else if (text[i] >= 65 && text[i] <= 77)        //if letter is between A and M (1 to 13)
+                {
+                    smallText += Convert.ToString(Convert.ToChar(text[i] + 13));     //Apply ROT13
+                }
+                else if (text[i] >= 78 && text[i] <= 90)      //if letter is between N and Z (14 to 26)
+                {
+                    smallText += Convert.ToString(Convert.ToChar(text[i] - 13));     //Apply ROT13
+                }
+                else if (text[i] == 45)      //if letter is -
+                {
+                    smallText += "-";
+                }
+
+                    if (text[i] == ' ')
+                {
+                    smallText += " ";
+                }
+            }
+
+            MessageBox.Show(smallText);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    spaces++;
+                    bigText += " ";
+                }
+                else
+                {
+                    smallText = Convert.ToString(text[i]);
+                    bigText += smallText;
+                }
+            }
+
+            MessageBox.Show(bigText + "... Spaces counter: " + spaces);
+        }
+    }
+}
+
+
+/*
+ * Check through all the text
+ * If there is a space, store die previous values in one string
+ * Carry on and check if more spaces, and repeat
+*/
